8b34f8cae35292a09ddd9e344e2418f234e00054
UglyNoob <1253328221@qq.com>
2020-05-09 19:29:27 +0800

    yeah

---
+++ my.cpp
@@ -0,0 +1,68 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "values.h"
+#include<SDL2/SDL_image.h>
+#include<SDL2/SDL.h>
+#include<cstdio>
+
+void exitGame(int);
+void outError(const char*);
+void gaming();
+void init(bool, bool);
+
+char stat;
+
+SDL_Window* window;
+SDL_Surface* screen;
+
+int main(int argc, char** argv) {
+    init(true, true);
+    gaming();
+    exitGame(0);
+    return 0;
+}
+
+void gaming() {
+    SDL_Surface* image=SDL_LoadBMP("tank/enemy2D.bmp");
+    SDL_Rect pos;
+    pos.width=100;
+    pos.height=100;
+    pos.y=250;
+    while(true){
+        pos.x++;
+        SDL_FillRect(screen,&pos,SDL_MapRGB(screen->format,0,0,255));
+        SDL_UpdateWindowSurface(window);
+    }
+}
+
+void init(bool initSDL, bool resetGame) {
+    if (initSDL) {
+        if (SDL_Init(SDL_INIT_EVENTS || SDL_INIT_VIDEO) <= -1) {
+            outError(SDL_GetError());
+            exit(-1);
+        }
+        window = SDL_CreateWindow("game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            outError(SDL_GetError());
+            exit(-1);
+        }
+        screen = SDL_GetWindowSurface(window);
+        if (screen == NULL) {
+            outError(SDL_GetError());
+            exit(-1);
+        }
+    }
+    if (resetGame) {
+        stat = STAT_MAINMENU;
+    }
+}
+
+void outError(const char *err) {
+    fprintf(stderr, "ERROR: %s", err);
+}
+void exitGame(int code) {
+    SDL_FreeSurface(screen);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    exit(code);
+}
---
+++ values.h
@@ -0,0 +1,9 @@
+#ifndef ___VALUES
+#define ___VALUES
+
+#define STAT_MAINMENU	'\0'
+#define STAT_GAMING		'\1'
+#define STAT_PAUSING	'\2'
+#define STAT_GAMEOVER	'\3'
+
+#endif
